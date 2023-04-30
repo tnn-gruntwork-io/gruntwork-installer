20e929b1eef59e5f71fcfdb4fb831d8b4689182d
purvakdevops <purvakdevops@gmail.com>
2023-04-30 15:49:56 -0500

    updated to tnn-gruntwork-io

--- bootstrap-gruntwork-installer.sh
+++ bootstrap-gruntwork-installer.sh
@@ -4,7 +4,7 @@
 #
 # Usage:
 #
-# curl -LsS https://raw.githubusercontent.com/gruntwork-io/gruntwork-installer/v0.0.38/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version v0.0.38
+# curl -LsS https://raw.githubusercontent.com/tnn-gruntwork-io/gruntwork-installer/v0.0.38/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version v0.0.38
 #
 # Rationale:
 #
@@ -51,7 +51,7 @@ function print_usage {
   echo "    bootstrap-gruntwork-installer.sh --version v0.0.38"
   echo
   echo "  One-liner to download this bootstrap script from GitHub and run it to install version v0.0.38:"
-  echo "    curl -Ls https://raw.githubusercontent.com/gruntwork-io/gruntwork-installer/v0.0.38/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version v0.0.38"
+  echo "    curl -Ls https://raw.githubusercontent.com/tnn-gruntwork-io/gruntwork-installer/v0.0.38/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version v0.0.38"
 }
 
 function maybe_sudo {
@@ -223,7 +223,7 @@ function warn {
   echo "--------------------------------------------------------------------------------------------------------------"
   echo "⚠️: Warning: You're installing this tool from the 'master' branch, which will be removed February 1, 2023."
   echo "   Please instead use a tagged release. E.g.:"
-  echo "   curl -Ls https://raw.githubusercontent.com/gruntwork-io/gruntwork-installer/v0.0.38/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version v0.0.38"
+  echo "   curl -Ls https://raw.githubusercontent.com/tnn-gruntwork-io/gruntwork-installer/v0.0.38/bootstrap-gruntwork-installer.sh | bash /dev/stdin --version v0.0.38"
   echo "--------------------------------------------------------------------------------------------------------------"
 }
 
